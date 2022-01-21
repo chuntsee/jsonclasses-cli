3a5a018d97f676222cb9d986cc560c474d35b4f9
Ben Wong <ben.wong@fillmula.com>
2022-01-21 16:50:51 +0800

    undo

--- tests/test_package_ts.py
+++ tests/test_package_ts.py
@@ -45,36 +45,36 @@ class TestPackageTs(TestCase):
 
     def test_package_create_without_link_and_session(self) -> None:
         package(self.temp_path, self.cls_dir / 'simple_song.py', 'ts', 'simple', True)
-        result = self.ts_path / 'src' / 'index.d.ts'
+        result = self.ts_path / 'src' / 'index.ts'
         expect = self.data_dir / 'simple_song_api.ts'
         self.assertEqual(result.read_text(), expect.read_text())
 
     def test_package_create_with_session(self) -> None:
         package(self.temp_path, self.cls_dir / 'session.py', 'ts', 'session', True)
-        result = self.ts_path / 'src' / 'index.d.ts'
+        result = self.ts_path / 'src' / 'index.ts'
         expect = self.data_dir / 'session_api.ts'
         self.assertEqual(result.read_text(), expect.read_text())
 
     def test_package_create_with_linkedthru_and_session(self) -> None:
         package(self.temp_path, self.cls_dir / 'linkedthru_session.py', 'ts', 'linkedthru_session', True)
-        result = self.ts_path / 'src' / 'index.d.ts'
+        result = self.ts_path / 'src' / 'index.ts'
         expect = self.data_dir / 'linkedthru_session_api.ts'
         self.assertEqual(result.read_text(), expect.read_text())
 
     def test_package_create_with_linkedthru(self) -> None:
         package(self.temp_path, self.cls_dir / 'linkedthru.py', 'ts', 'linkedthru', True)
-        result = self.ts_path / 'src' / 'index.d.ts'
+        result = self.ts_path / 'src' / 'index.ts'
         expect = self.data_dir / 'linkedthru_api.ts'
         self.assertEqual(result.read_text(), expect.read_text())
 
     def test_package_create_with_linkto_and_session(self) -> None:
         package(self.temp_path, self.cls_dir / 'linkto_session.py', 'ts', 'linkto_session', True)
-        result = self.ts_path / 'src' / 'index.d.ts'
+        result = self.ts_path / 'src' / 'index.ts'
         expect = self.data_dir / 'linkto_session_api.ts'
         self.assertEqual(result.read_text(), expect.read_text())
 
     def test_package_create_with_linkto(self) -> None:
         package(self.temp_path, self.cls_dir / 'linkto.py', 'ts', 'linkto', True)
-        result = self.ts_path / 'src' / 'index.d.ts'
+        result = self.ts_path / 'src' / 'index.ts'
         expect = self.data_dir / 'linkto_api.ts'
         self.assertEqual(result.read_text(), expect.read_text())
